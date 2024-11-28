00d9fc7dd8af96918b8f3ee92021cd5d48fbc44d
RobertoJuniorAndarilho <168129503+RobertoJuniorAndarilho@users.noreply.github.com>
2024-11-28 03:23:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Musica',
+    'O que é o Rock?',
+    'O rock é um gênero musical com ritmo forte, guitarras elétricas, bateria potente e vocais expressivos, originado nos EUA nos anos 50.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Alagoas?',
+    'A capital do Alagoas é Maceió.'
+)
+
+criaCartao(
+    'Música',
+    'O que é um instrumento de corda?',
+    'Instrumento musical que produz som através da vibração de cordas, como violão, guitarra, harpa e violino.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz de nada em Inglês?',
+    'De nada em ingles é You are Welcome'
+)
